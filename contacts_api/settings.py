09c9ad72a9b4750911a1808ca00956fce50fb0b7
pouya <lotfiyan.p77@gamil.com>
2021-06-05 17:15:54 +0430

    default database chaged to postgres local

--- contacts_api/settings.py
+++ contacts_api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contacts.app.ContactsConfig'
+    'contacts.apps.ContactsConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,9 +75,24 @@ WSGI_APPLICATION = 'contacts_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'p.ltf1377&&#!',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
