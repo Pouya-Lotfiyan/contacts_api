0951dfbbf3389a5a281f98520ca72697df63446a
pouya <lotfiyan.p77@gamil.com>
2021-06-05 18:50:44 +0430

    basic view created

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .serializers import ContactSerializer
+from .models import Contact
+from rest_framework import generics
 
-# Create your views here.
+class ContactsList(generics.ListCreateAPIView):
+    queryset = Contact.objects.all().order_by('first_name')
+    serializer_class = ContactSerializer
+
+
+class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Contact.objects.all().order_by('first_name')
+    serializer_class = ContactSerializer
