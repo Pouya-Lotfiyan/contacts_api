9c0f74c949e9bd6dc8e1738d49b8e0d3fd4a5a83
pouya <lotfiyan.p77@gamil.com>
2021-06-05 17:14:15 +0430

    contact model added

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    first_name = models.CharField("first_name", max_length=50)
+    last_name = models.CharField("last_name", max_length=50)
+    postal_code = models.CharField("postal_code", max_length=50)
+    city = models.CharField("city", max_length=50)
+    created_at = models.DateTimeField("created_at", auto_now_add=True)
+    update_at = models.DateTimeField("updated_at", auto_now=True)
+
+    class Meta:
+        verbose_name = "Contact"
+        verbose_name_plural = "Contacts"
+
+    def __str__(self):
+        return self.name
+
+
