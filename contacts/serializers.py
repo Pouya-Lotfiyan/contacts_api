1a104c3149bef7bbf46f80e5fdbcafdf8827be7c
pouya <lotfiyan.p77@gamil.com>
2021-06-05 23:39:05 +0430

    add phone numbers into contact response

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -1,10 +1,20 @@
 
 
 from rest_framework import serializers
-from .models import Contact
+from .models import Contact, PhoneNumber
 
+
+
+
+class PhoneNumberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhoneNumber
+        fields = ('id', 'type', 'number', 'contact')
 
 class ContactSerializer(serializers.ModelSerializer):
+
+    phoneNumbers = PhoneNumberSerializer(many=True)
+
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name')
+        fields = ('id', 'first_name', 'last_name', 'phoneNumbers')
